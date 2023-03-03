1182c5a0136e16e6cba92f1f0953b2baaf75ba9f
Arkadiusz Pucek <102652805+ArkadiuszPucek@users.noreply.github.com>
2023-03-03 13:33:29 +0100

    Update FileManagerBuilder.java

--- src/pl/CampMads/io/file/FileManagerBuilder.java
+++ src/pl/CampMads/io/file/FileManagerBuilder.java
@@ -1,46 +1,44 @@
 package pl.CampMads.io.file;
 
-
-import pl.CampMads.exeption.SoSuchFileTypeException;
+import pl.CampMads.exception.UnsupportedFileTypeException;
 import pl.CampMads.io.ConsolePrinter;
 import pl.CampMads.io.DataReader;
 
-
 public class FileManagerBuilder {
-    private ConsolePrinter printer;
-    private DataReader reader;
+    private final ConsolePrinter printer;
+    private final DataReader reader;
 
     public FileManagerBuilder(ConsolePrinter printer, DataReader reader) {
         this.printer = printer;
         this.reader = reader;
     }
 
-    public FileManager build(){
+    public FileManager build() {
         printer.printLine("Wybierz format danych:");
         FileType fileType = getFileType();
-        switch (fileType){
+        switch (fileType) {
             case SERIAL:
                 return new SerializableFileManager();
             case CSV:
                 return new CsvFileManager();
             default:
-                throw new SoSuchFileTypeException("Nieobsługiwany typ danych");
+                throw new UnsupportedFileTypeException("Nieobsługiwany typ danych");
         }
     }
 
     private FileType getFileType() {
         boolean typeOk = false;
         FileType result = null;
-        do{
+        do {
             printTypes();
             String type = reader.getString().toUpperCase();
-            try{
+            try {
                 result = FileType.valueOf(type);
                 typeOk = true;
-            } catch (IllegalArgumentException e){
-                printer.printLine("Nieobługiwany typ danych, wybierz ponownie");
+            } catch (IllegalArgumentException e) {
+                printer.printLine("Nieobsługiwany typ danych, wybierz ponownie");
             }
-        }while (!typeOk);
+        } while (!typeOk);
         return result;
     }
 
